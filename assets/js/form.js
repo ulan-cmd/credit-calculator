0c7236cd114a28c83d63b60c427f2023ebce7587
ulan-cmd <ulan22.work@gmail.com>
2020-06-17 20:58:31 +0600

    change form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,6 @@
 let data = localStorage.getItem('data');
 data = JSON.parse(data);
-console.log(data.summa);
-console.log(data);
+
+document.getElementById('num1').textContent = `${data.summa} сомов`;
+document.getElementById('num2').textContent = `${data.srok} месяцев`;
+document.getElementById('num3').textContent = `${data.summaVozvrata} сомов`;
